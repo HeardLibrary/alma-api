8bb2f3f0500fbb0a047e7fd92af56016c6cf56f4
tao <tao.you@vanderbilt.edu>
2020-04-14 15:38:13 -0500

    Updates to user api functions, and completed the script to process postdoc records

--- sync_postdoc_json.php
+++ sync_postdoc_json.php
@@ -1,19 +1,18 @@
 <?php
-/** This script generate user json file, sync and update user record in Alma **/ 
-
 /*************************************************************************
-* read postdoc.csv file, grab user primary ID;
-*    foreach (primaryID) {
-*        if loaded in Alma {
+ This script read a list of users provided by Office of Postdoctroal Affair, check again Alma to update or create user records to reflect their postdoc status in Alma 
+
+read postdoc.csv file, grab user primary ID;
+    foreach (primaryID) {
+        if already loaded in Alma {
             retrieve full user_json using user API get
-            update user_json
-*           post user_json back to user record using user API PUT
-         } else {
+            update user_json to posdoc record 
+           post user_json back to user record using user API PUT
+        } else {
             generate user_json based on csv content
             create user record using usesr API POST 
-         }
-
-*   }
+        }
+    }
 *************************************************************************/
 
 //output outcome as it is generated
@@ -34,7 +33,6 @@ $apikey = $apikeys[$server][$keytype];
 echo "<p><strong> you are running the script on $server </strong></p>";
 
 $input_fname = "user_data/active_VUMC_postdocs.csv"; 
-/* file format is: Full Name,Last Name,First Name,VUMC ID,School,Unit,Title,Is Active?,Appt Start Date,Appt End Date,E-mail Address (VU),E-mail Address (VUMC),Entity,Hire Date  */
 
 //open csv to read
 $infile = fopen($input_fname, 'rt'); 
@@ -43,68 +41,82 @@ if (!$infile) { echo "cannot open input file"; exit; }
 //get the headers of the file 
 $headers = fgetcsv($infile);  //not processing csv header at this time
 
-$t = 0; $i = 0; $j = 0; $k = 0;  
+$cnt_total = 0; $cnt_updated = 0; $cnt_created = 0; $cnt_errored = 0;  
+
 while (($row = fgetcsv($infile)) !== FALSE) {
     //testing control 
-    if ($t < 3) {$t++; continue;}  
-
-    if ($t >7) break;
+    if ($cnt_total < 10) {$cnt_total++; continue;}  
+    if ($cnt_total > 15) break;
 
     $primary_id = $row[3]; 
 
-    $rr = curl_get_user_details($primary_id, $apikey);
+    $r_get = curl_get_user_details($primary_id, $apikey);
 
-    if ( !isset( json_decode($rr)->errorsExist) ) { // user retrieved successfully 
+    if ( !isset( json_decode($r_get)->errorsExist) ) { // user retrieved successfully 
        
-        echo $rr, "<br/>"; 
-        $user = json_decode($rr); 
+        //echo $rr, "<br/>"; 
+        $user = json_decode($r_get); 
         //var_dump($user);  
 
         $ugroup = $user->user_group; 
         echo $primary_id, " -- ", $ugroup->value;
 
-        $new_ugroup = array("value" => "FACULTY", "desc" => "Faculty");
-        update_user_json($user, "user_group", $new_ugroup);
-        //var_dump($updated_user->user_group); 
+        $user = update_postdoc_user($user); 
+        
+        $r_update = curl_update_user($primary_id, $user, $apikey); 
 
-        $rr = curl_update_user($primary_id, $user, $apikey); 
-
-        if ( isset(json_decode($rr)->errorsExist) ) {
-            $k++; echo " --- error <br/>"; 
+        if ( isset(json_decode($r_update)->web_service_result->errorsExist) ) { 
+            $cnt_errored ++; 
+            echo " --- error <br/>"; 
         } 
         else {
-            $i++; echo "-- Done <br/>"; 
+            $cnt_updated ++; 
+            echo " -- Done <br/>"; 
         }
     }
     else { // user retrieve unsuccessful
 
-        if (json_decode($rr)->errorList->error[0]->errorCode == "401861") {
-            echo $primary_id, " -- user not in the system, create one; ";
+        if (json_decode($r_get)->errorList->error[0]->errorCode == "401861") {
+            echo $primary_id, " -- user not in the system, create one... ";
         
-            $user_json = create_postdoc_json_from_csv($row); 
-            $rr = curl_create_user($user_json, $apikey);
+            $user_new = create_postdoc_json_from_csv($row);
+
+            $r_create = curl_create_user($user_new, $apikey);
+            //echo "<br/>r_create", $r_create; 
             
-            if ( isset(json_decode($rr)->errorsExist) ) {
-                $k++; echo " --- error <br/>"; 
+            if ( isset(json_decode($r_create)->web_service_result->errorsExist) ) {
+                $cnt_errored ++; 
+                echo " --- error <br/>"; 
             } else {
-                $j++; echo "-- Done <br/>"; 
+                $cnt_created ++; 
+                echo " -- Done <br/>"; 
             }
 
         }
         else{
-            $k ++;  
+            $cnt_errored ++;  
             echo $primary_id, " -- something wrong with the user record<br/>"; 
         }    
     }
     
-    $t ++; 
+    $cnt_total ++; 
 }          
 
-echo "<p>$i records updated; $j records created; $k records errored out; </p>"; 
+echo "<p>$cnt_updated records updated; $cnt_created records created; $cnt_errored records errored out; </p>"; 
 
 
 function create_postdoc_json_from_csv($row){
-//update user_json string using row data read from .csv file 
+/**** update user_json using row data read from .csv file 
+file format is: Full Name,Last Name,First Name,VUMC ID,School,Unit,Title,Is Active?,Appt Start Date,Appt End Date,E-mail Address (VU),E-mail Address (VUMC),Entity,Hire Date  
+
+* status_date: today
+* first_name, last_name, full_name, primary_id (vumc_id)
+* address: unit from .csv file
+* emails: vu_email, vumc_email (first empty is preferred)
+* user_group: FACULTY
+* user_statistics: POSTDOC
+* user_note: VUMC postdoc user loaded by LTDS
+**********************************************************/
 
     $postdoc_string = file_get_contents("user_patron.json"); 
     $user = json_decode($postdoc_string); 
@@ -128,13 +140,13 @@ function create_postdoc_json_from_csv($row){
     $postdoc_user_group = array("value" => "FACULTY", "desc" => "Faculty"); 
     update_user_json($user, "user_group", $postdoc_user_group);
 
-    $postdoc_addresses = array( array('line1'=>$row[6], 'line2'=>$row[5]) );
+    $postdoc_addresses = array( array('line1'=>$row[5], 'line2'=>$row[4]) );
     $addresses = create_addresses_json($postdoc_addresses); 
     update_user_json($user, "contact_info->address", $addresses);
 
     $postdoc_emails = array( 
-        array("email_value" => $row[10], "email_desc" => "vu email address", "email_types" => array("school"=>"School")), 
-        array("email_value" => $row[11], "email_desc" => "VUMC email", "email_types"=> array("work"=>"Work"))
+        array("email_value" => $row[11], "email_desc" => "VUMC email address", "email_types" => array("school"=>"School")), 
+        array("email_value" => $row[10], "email_desc" => "VU email", "email_types"=> array("alternative"=>"Alternative"))
     ); 
     $emails = create_emails_json($postdoc_emails); 
     //print_r($emails);
@@ -143,24 +155,48 @@ function create_postdoc_json_from_csv($row){
     $postdoc_user_stats = array( 
         array("statistic_category" => array( "value"=>"POSTDOC", "desc"=>"Postdocs"),
               "segment_type" => "Internal" ) ); 
-    update_user_json($user, "user_statistics", $postdoc_user_stats); 
+    update_user_json($user, "user_statistic", $postdoc_user_stats); 
 
     $postdoc_user_notes = array(
         array('note_type'=> array("value"=>"OTHER", "desc"=>"Other"), 
               'note_text' => "VUMC postdoc user loaded by LTDS", 
-              'note_viewable'=> true, 
-              'popup_note' => false, 
-              'create_by'=> "LTDS", 
-              'segment_type' => "Internal", 
-              'create_date' => "" )
-    ); 
-    // foreach($postdoc_user_notes as $note) {
-    //     $note['create_date'] = date('Y-m-d')."T11:00:00Z"; 
-    // }
+              'user_viewable'=> true, 
+              'popup_note' => false
+          ) ); 
     update_user_json($user, "user_note", $postdoc_user_notes);  
     
-    $user_json = json_encode($user);
-    return $user_json; 
+    return $user; 
+}
+
+
+function update_postdoc_user( &$user) {
+/****update postdoc user records **************
+* status_date: today
+* user_group: Faculty
+* user_statistic: POSTDOC
+* user_note: VUMC postdoc user updated by LTDS 
+************************************************/  
+
+    $status_date = date('Y-m-d'). "Z"; 
+    update_user_json($user, "status_date", $status_date); 
+
+    $new_ugroup = array("value" => "FACULTY", "desc" => "Faculty");
+    update_user_json($user, "user_group", $new_ugroup);
+
+    $postdoc_user_stats = array( 
+        array("statistic_category" => array( "value"=>"POSTDOC", "desc"=>"Postdocs"),
+              "segment_type" => "Internal" ) ); 
+    update_user_json($user, "user_statistic", $postdoc_user_stats); 
+
+    $postdoc_user_notes = array(
+        array('note_type'=> array("value"=>"OTHER", "desc"=>"Other"), 
+              'note_text' => "VUMC postdoc user updated by LTDS", 
+              'user_viewable'=> true, 
+              'popup_note' => false
+        ) ); 
+    update_user_json($user, "user_note", $postdoc_user_notes);
+
+    return $user;   
 }
 
 ?>
