58c36da834fb18f8b2f2650a5f721d5354fa6bee
Jamen McGranahan <jamen.mcgranahan@vanderbilt.edu>
2020-04-22 11:41:57 -0500

    Inactivations SFTP

--- inactivations.php
+++ inactivations.php
@@ -1,4 +1,6 @@
 <?php
+use phpseclib\Net\SFTP;
+
 // Grab SFTP info
 include('ftp_info.php');
 
@@ -6,11 +8,7 @@ include('ftp_info.php');
 $vu=vu();
 $vumc=vumc();
 
-if (ftp_chdir($vu, "inbound/prd/alma-inactivations")) {
-	if (ftp_get($vu, "inactives.zip", "ils_student_inactive_export.zip", FTP_BINARY)) {
-		echo "Successfully downloaded file.";
-	} else {
-		echo "There was a problem downloading the file.";
-	}
-}
+$sftp->chdir('inbound/prod/alma-inactivations');
+$sftp->get('ils_student_inactive_export.zip','vu_inactives.zip');
+
 ?>
