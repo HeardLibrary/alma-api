f573f0ba8b0009ab144558af384bdf1b63e48bc1
Jamen McGranahan <jamen.mcgranahan@vanderbilt.edu>
2020-04-23 09:50:12 -0500

    Update inactivations.php
    removing processing of XML file

--- inactivations.php
+++ inactivations.php
@@ -15,9 +15,9 @@ $vu=vu();
 $vu->chdir('inbound/prd/alma-inactivations');
 $vu->get('ils_student_inactive_export.zip','vu_inactives.zip');
 exec('unzip vu_inactives.zip');
-exec('cat ils_student_inactive_export.xml|grep primary_id|sed "s/<primary_id>//g"|sed "s/<\/primary_id>//g"|sed -e "s/ //g" > inactives.txt');
+//exec('cat ils_student_inactive_export.xml|grep primary_id|sed "s/<primary_id>//g"|sed "s/<\/primary_id>//g"|sed -e "s/ //g" > inactives.txt');
 rename('vu_inactives.zip','user_data/Archive/vu_inactives_'.$datevar.'.zip');
-unlink('ils_student_inactive_export.xml');
+//unlink('ils_student_inactive_export.xml');
 
 
 // VUMC FTP connector
