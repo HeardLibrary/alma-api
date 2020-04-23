7af9d4d7cdff9cd8ac6097f8338b1cc0f514b38b
Jamen McGranahan <jamen.mcgranahan@vanderbilt.edu>
2020-04-23 14:25:53 -0500

    Update inactivations.php
    add Tao's PHP script

--- inactivations.php
+++ inactivations.php
@@ -15,17 +15,18 @@ $vu=vu();
 $vu->chdir('inbound/prd/alma-inactivations');
 $vu->get('ils_student_inactive_export.zip','vu_inactives.zip');
 exec('unzip vu_inactives.zip');
-//exec('cat ils_student_inactive_export.xml|grep primary_id|sed "s/<primary_id>//g"|sed "s/<\/primary_id>//g"|sed -e "s/ //g" > inactives.txt');
-expire_inactive_users.php?server=production&infile=ils_student_inactive_export.xml
 rename('vu_inactives.zip','user_data/Archive/vu_inactives_'.$datevar.'.zip');
+exec('php expire_inactive_users.php?server=sandbox&infile=ils_student_inactive_export.xml');
 //unlink('ils_student_inactive_export.xml');
 
 
 // VUMC FTP connector
-// $vumc=vumc();
+$vumc=vumc();
 
-// $vumc->chdir('inactivate');
-// $vumc->get('en_library_inactivate.medc.xml.zip','vumc_inactives.zip');
-// exec('unzip vumc_inactives.zip');
-// rename('vumc_inactives.zip','user_data/Archive/vumc_inactives_'.$datevar.'.zip');
+$vumc->chdir('inactivate');
+$vumc->get('en_library_inactivate.medc.xml.zip','vumc_inactives.zip');
+exec('unzip vumc_inactives.zip');
+rename('vumc_inactives.zip','user_data/Archive/vumc_inactives_'.$datevar.'.zip');
+exec('php expire_inactive_users.php?server=sandbox&infile=en_library_inactivate.medc.xml');
+
 ?>
