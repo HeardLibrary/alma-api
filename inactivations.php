e0b52b0b82f0955b7814d639cd1ce0c8ae66d50f
Jamen McGranahan <jamen.mcgranahan@vanderbilt.edu>
2020-04-23 09:01:22 -0500

    Update inactivations.php
    added missing ;

--- inactivations.php
+++ inactivations.php
@@ -13,7 +13,7 @@ $vu->chdir('inbound/prd/alma-inactivations');
 $vu->get('ils_student_inactive_export.zip','user_data/vu_inactives.zip');
 exec('cd user_data');
 exec('unzip vu_inactives.zip');
-exec('cat ils_student*.xml|grep primary_id|sed "s/<primary_id>//g"|sed "s/<\/primary_id>//g"|sed -e "s/ //g" > inactives.txt')
+exec('cat ils_student*.xml|grep primary_id|sed "s/<primary_id>//g"|sed "s/<\/primary_id>//g"|sed -e "s/ //g" > inactives.txt');
 exec('cd ..');
 
 // VUMC FTP connector
