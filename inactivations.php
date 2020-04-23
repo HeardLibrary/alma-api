babbdf57e769e0ffbff9e3d7e8beaf1401b4e6dc
Jamen McGranahan <jamen.mcgranahan@vanderbilt.edu>
2020-04-23 09:17:16 -0500

    Update inactivations.php
    renaming downloaded file

--- inactivations.php
+++ inactivations.php
@@ -6,6 +6,9 @@ use phpseclib\Net\SFTP;
 // Grab SFTP info
 include('ftp_info.php');
 
+// create date variable to be used for archiving
+$datevar=date("Ymd");
+
 // VU FTP connector
 $vu=vu();
 
@@ -13,11 +16,12 @@ $vu->chdir('inbound/prd/alma-inactivations');
 $vu->get('ils_student_inactive_export.zip','vu_inactives.zip');
 exec('unzip vu_inactives.zip');
 exec('cat ils_student_inactive_export.xml|grep primary_id|sed "s/<primary_id>//g"|sed "s/<\/primary_id>//g"|sed -e "s/ //g" > inactives.txt');
+rename('vu_inactives.zip','user_data/Archive/vu_inactives_$datevar.zip');
 
 // VUMC FTP connector
-$vumc=vumc();
+//$vumc=vumc();
 
-$vumc->chdir('files/inactivate');
-$vumc->get('en_library_inactivate.medc.xml.zip','vumc_inactives.zip');
-exec('unzip vumc_inactives.zip');
+//$vumc->chdir('files/inactivate');
+//$vumc->get('en_library_inactivate.medc.xml.zip','vumc_inactives.zip');
+//exec('unzip vumc_inactives.zip');
 ?>
