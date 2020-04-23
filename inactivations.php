c3c949027ca352465a081788a8e525849382c55b
Jamen McGranahan <jamen.mcgranahan@vanderbilt.edu>
2020-04-23 09:00:33 -0500

    Update inactivations.php
    processing downloaded file

--- inactivations.php
+++ inactivations.php
@@ -6,11 +6,21 @@ use phpseclib\Net\SFTP;
 // Grab SFTP info
 include('ftp_info.php');
 
-// FTP connector
+// VU FTP connector
 $vu=vu();
-//$vumc=vumc();
 
 $vu->chdir('inbound/prd/alma-inactivations');
 $vu->get('ils_student_inactive_export.zip','user_data/vu_inactives.zip');
+exec('cd user_data');
+exec('unzip vu_inactives.zip');
+exec('cat ils_student*.xml|grep primary_id|sed "s/<primary_id>//g"|sed "s/<\/primary_id>//g"|sed -e "s/ //g" > inactives.txt')
+exec('cd ..');
 
+// VUMC FTP connector
+$vumc=vumc();
+
+$vumc->chdir('files/inactivate');
+$vumc->get('en_library_inactivate.medc.xml.zip','user_data/vumc_inactives.zip');
+exec('cd user_data');
+exec('unzip vumc_inactives.zip');
 ?>
