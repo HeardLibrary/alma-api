b22cd45d05f47f2fae1f79ff488fb97395bad349
Jamen McGranahan <jamen.mcgranahan@vanderbilt.edu>
2020-04-24 12:19:41 -0500

    Update inactivations.php
    full path to PHP

--- inactivations.php
+++ inactivations.php
@@ -16,7 +16,7 @@ $vu->chdir('inbound/prd/alma-inactivations');
 $vu->get('ils_student_inactive_export.zip','vu_inactives.zip');
 exec('unzip vu_inactives.zip');
 rename('vu_inactives.zip','user_data/Archive/vu_inactives_'.$datevar.'.zip');
-exec('expire_inactive_users.php server=sandbox&infile=vu');
+exec('/opt/remi/php74/root/usr/bin/php expire_inactive_users.php server=sandbox&infile=vu');
 unlink('ils_student_inactive_export.xml');
 
 // VUMC FTP connector
@@ -26,7 +26,7 @@ $vumc->chdir('inactivate');
 $vumc->get('en_library_inactivate.medc.xml.zip','vumc_inactives.zip');
 exec('unzip vumc_inactives.zip');
 rename('vumc_inactives.zip','user_data/Archive/vumc_inactives_'.$datevar.'.zip');
-exec('expire_inactive_users.php server=sandbox&infile=vumc');
+exec('/opt/remi/php74/root/usr/bin/php expire_inactive_users.php server=sandbox&infile=vumc');
 unlink('en_library_inactivate.medc.xml');
 
 ?>
