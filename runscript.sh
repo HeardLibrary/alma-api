c954d89395e57563c386d722eb86b0c33acd6544
Jamen McGranahan <jamen.mcgranahan@vanderbilt.edu>
2020-04-24 12:43:24 -0500

    Create runscript.sh
    adding bash script

---
+++ runscript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+HOSTNAME=libvm15.library.vanderbilt.edu; export HOSTNAME
+SHELL=/bin/bash; export SHELL
+TERM=xterm; export TERM
+USER=root; export USER
+LD_LIBRARY_PATH=/opt/remi/php74/root/usr/lib64; export LD_LIBRARY
+PATH=/usr/local/rvm/gems/ruby-2.6.3/bin:/usr/local/rvm/gems/ruby-2.6.3@global/bin:/usr/local/rvm/rubies/ruby-2.6.3/bin:/usr/lib64/qt-3.3/bin:/opt/remi/php74/root/usr/bin:/opt/remi/php74/root/usr/sbin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/rvm/bin:/root/bin; export PATH
+LANG=en_US.UTF-8; export LANG
+
+datevar=date('Ymd')
+
+cd /apps/alma/alma-api
+
+php inactivations.php
+
+php expire_inactive_users.php server=sandbox&infile=vu
+
+php expire_inactive_users.php server=sandbox&infile=vumc
+
+##mv vu_inactives.zip user_data/Archive/vu_inactives_$datevar.zip
+
+##mv vumc_inactives.zip user_data/Archive/vumc_inactives_$datevar.zip
+
+##rm ils_student_inactive_export.xml
+
+##rm en_library_inactivate.medc.xml
